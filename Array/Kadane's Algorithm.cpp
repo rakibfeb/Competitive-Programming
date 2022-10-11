886d5990b30cedeaceb824288b04ba3f20e626f1
rakibfeb <rakib8feb@gmail.com>
2022-10-11 19:51:16 +0530

    Kadane's Algorithm

---
+++ Array/Kadane's Algorithm.cpp
@@ -0,0 +1,16 @@
+ long long maxSubarraySum(int arr[], int n){
+            long long  maxSum = INT_MIN;
+            long long currSum = 0;
+            
+            for (int i = 0; i < n ; i++) {
+                 currSum += arr[i];
+                 if (currSum > maxSum) {
+                     maxSum = currSum;
+                 }
+                 if(currSum < 0){
+                     currSum = 0;
+                 }
+            }
+    return maxSum; 
+        
+    }
