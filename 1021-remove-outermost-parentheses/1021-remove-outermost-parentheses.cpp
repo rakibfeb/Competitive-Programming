b17cbcc5aa60ae93c5e3821d34367204dc9148ff
Rakib Mondal <112157068+rakibfeb@users.noreply.github.com>
2023-01-04 20:42:41 +0530

    Time: 173 ms (5.07%), Space: 253.8 MB (5.07%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) 
+    {
+        int flag = 0,l = int(s.length());
+        string result;
+        for(int i = 0;i < l;i++)
+        {
+            if(s[i] == '(')
+            {
+                flag += 1;
+                
+                //  result += flag > 1 ? "(" : "";
+                if(flag < 2)
+                {
+                    result = result+"";
+                }
+                else
+                {
+                    result = result + "(";
+                }
+            
+            } else 
+            {
+                flag -= 1;
+                
+                //result += flag > 0 ? ")" : "";
+                if(flag < 1)
+                {
+                    result = result+"";
+                }
+                else
+                {
+                    result = result + ")";
+                }                            
+            }
+            
+        }
+        return result;
+    }
+};
