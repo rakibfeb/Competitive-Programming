dd51064c5e84a795de9f3db3aa911853a1e2a42c
Rakib Mondal <112157068+rakibfeb@users.noreply.github.com>
2023-01-05 21:43:14 +0530

    Time: 38 ms (9.10%), Space: 8.1 MB (67.65%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,18 +9,25 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* slow = head;
-        if(head == NULL || head->next == NULL ) return false;
-        ListNode* fast = head->next;
-        while(slow != fast )
-        {
-            //if(fast-> == NULL) return 
-            if(fast->next != NULL && fast->next->next != NULL)
-                fast = fast->next->next;
-            else
-                return false;
-            slow = slow->next;
+         // edge case 
+        if(head == NULL || head->next == NULL || head->next->next == NULL) return false;
+        // take animals to the trackline
+        ListNode* turtle = head;
+        ListNode* rabbit = head;
+        // detect is there any cycle or not 
+        while(rabbit->next != NULL && rabbit->next->next != NULL){
+            rabbit = rabbit->next->next;
+            turtle = turtle->next;
+            // check if turtle and the rabbit at the same place or not 
+            // if at the same place then stop the race
+            if(turtle == rabbit)
+                break;
         }
-       return true; 
+        
+        // check if they are not not at the same place it means rabbit reached the              end line, so there have no cycle 
+        if(turtle != rabbit )
+            return false;
+        
+        return true;
     }
 };
