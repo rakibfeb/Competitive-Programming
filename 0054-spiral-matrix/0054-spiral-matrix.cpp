1b3c782ed2a8374f373eea70c320143a6fd35ec9
Rakib Mondal <112157068+rakibfeb@users.noreply.github.com>
2023-01-12 13:40:46 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (92.56%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -12,18 +12,22 @@ public:
                 ans.push_back(matrix[top][i]);
             }
             top++;
+            
+            // for this taste case [[1,2,3,4],[5,6,7,8],[9,10,11,12]] output will be [1,2,3,4,8,12,11,10,9,5,6,7] but with out this if the ouput is [1,2,3,4,8,12,11,10,9,5,6,7,6]
+            if(top > buttom) break;
+            
             // top --> buttom
             // right fixed
-            if(top > buttom){
-                break;
-            }
             for(int i = top; i<= buttom ; i++){
                 ans.push_back(matrix[i][right]);
             }
             right--;
+            //for this taste case [[7],[9],[6]] output will be [7,9,6] but without this if the output is [7,9,6,9]
+            
+            if(right < left) break;
+            
             // right --> left
             // buttom fixed
-            if(right < left) break;
             for(int i = right ; i >= left ; i--){
                 ans.push_back(matrix[buttom][i]);
             }
