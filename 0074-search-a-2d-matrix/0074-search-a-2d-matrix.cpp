d6eaa47093bc068de918b4016c6b9850b1827ed3
Rakib Mondal <112157068+rakibfeb@users.noreply.github.com>
2023-01-28 19:14:32 +0530

    Time: 9 ms (29.59%), Space: 9.4 MB (80.06%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,17 +1,51 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int rows = matrix.size(),
-			cols = matrix[0].size(),
-            row = 0, col = cols - 1;
-			
-        while (row < rows && col > -1) {
-            int cur = matrix[row][col];
-            if (cur == target) return true;
-            if (target > cur) row++;
-            else col--;
+        int row = 0;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        // if(m ==1 and n ==1)
+        // {
+        //     if(matrix[0][0] == target) return true;
+        //     else return false;
+        // }
+        int start = 1, end = m-1;
+        if(matrix[0][n-1] > target) row = 0;
+        while(start <= end && matrix[0][n-1] < target && m > 1)
+        {
+            int mid = start + (end-start)/2;
+            cout<<matrix[mid][n-1]<<" and "<<matrix[mid-1][n-1]<<endl;
+            if(target <= matrix[mid][n-1] && matrix[mid-1][n-1] < target)
+            {
+                row = mid;
+              cout<<"row updated"<<endl;
+                break;
+            }
+            
+            else if(matrix[mid][n-1] < target )
+            {
+                start = mid+1;
+            }
+            else
+            {
+                end =mid-1;
+            }
         }
+        cout<<"row = "<<row<<endl;
+        if(row == -1) return false;
+        start = 0, end = n-1;
         
+        while(start <= end)
+        {
+            int mid = start+(end-start)/2;
+            if(matrix[row][mid] == target)return true;
+            else if(target > matrix[row][mid] ){
+                start = mid+1;
+            }
+            else{
+                end =mid-1;
+            }
+        }
         return false;
     }
 };
