1b1d0c4818e21a8a50d655d80d211e5b652c8e3c
Rakib Mondal <112157068+rakibfeb@users.noreply.github.com>
2023-02-19 21:50:39 +0530

    Time: 3 ms (73.66%), Space: 12.2 MB (48.40%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(!root)
+            return ans;
+        deque<TreeNode*> dq;
+        dq.push_back(root);
+        bool isLTR = true;
+        while(!dq.empty()) {
+            int n = dq.size();
+            vector<int> currLevel;
+            if(isLTR) {
+                isLTR = false;
+                for(int i=0;i<n;++i) {
+                    auto curr = dq.front();
+                    dq.pop_front();
+                    currLevel.push_back(curr->val);
+                    if(curr->left)
+                        dq.push_back(curr->left);
+                    if(curr->right)
+                        dq.push_back(curr->right);
+                }
+            } else {
+                isLTR = true;
+                for(int i=0;i<n;++i) {
+                    auto curr = dq.back();
+                    dq.pop_back();
+                    currLevel.push_back(curr->val);
+                    if(curr->right)
+                        dq.push_front(curr->right);
+                    if(curr->left)
+                        dq.push_front(curr->left);
+                }
+            }
+            ans.push_back(currLevel);
+        }
+        return ans;
+    }
+};
